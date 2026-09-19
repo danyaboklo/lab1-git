1236f34280ee0b0c5604e60c40931b92ab470481
Boklo Danila <danyaboklo@gmail.com>
2026-09-19 16:28:49 +0700

    Первый коммит: меню и функция варианта 2

---
+++ main.cpp
@@ -0,0 +1,51 @@
+// Лабораторная работа № 1. Вариант 2.
+// Выполнил: Бокло Д. А., группа ПИ-53.
+
+#include <iostream>
+
+using namespace std;
+
+// ===== Расчётные функции варианта =====
+
+// площадь квадрата
+double calculateSquare(double a)
+{
+    return a * a;
+}
+
+// ===== Главная функция: меню =====
+
+int main() {
+
+int choice;
+double x;
+
+do {
+cout << "\n=== Вариант 2: расчёты для квадрата ===\n";
+cout << "1. Площадь\n";
+cout << "0. Выход\n";
+
+cout << "Выберите пункт: ";
+cin >> choice;
+
+switch (choice) {
+
+case 1:
+cout<<"Введите число: ";
+cin>>x;
+cout<<"Результат: "
+<<calculateSquare(x)<<endl;
+break;
+
+case 0:
+cout << "Работа завершена.\n";
+break;
+
+default:
+cout << "Такого пункта нет.\n";
+}
+
+} while (choice != 0);
+
+return 0;
+}
